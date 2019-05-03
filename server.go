22b4a09c0670c21508687a1fdc0be324660e24f4
Chase Adams <realchaseadams@gmail.com>
2019-05-03 10:34:17 -0700

    run gofmt over server.go

--- server.go
+++ server.go
@@ -2,22 +2,22 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
- 
- package main
-  
- import (
-     "io"
-     "net/http"
-     "fmt"
- )
-  
- func hello(w http.ResponseWriter, r *http.Request) {
-     io.WriteString(w, "Hello remote world!")
- }
-  
- func main() {
-     portNumber := "9000"
-     http.HandleFunc("/", hello)
-     fmt.Println("Server listening on port ", portNumber)
-     http.ListenAndServe(":" + portNumber, nil)
-    }
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello remote world!")
+}
+
+func main() {
+	portNumber := "9000"
+	http.HandleFunc("/", hello)
+	fmt.Println("Server listening on port ", portNumber)
+	http.ListenAndServe(":"+portNumber, nil)
+}

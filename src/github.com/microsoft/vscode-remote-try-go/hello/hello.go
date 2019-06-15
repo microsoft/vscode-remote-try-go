378e01a632c9c4c3133551888368245356bb2a5b
Alexander Krieg <alitari67@gmail.com>
2019-06-15 13:36:53 +0200

    Add struct for demonstration fillstruct

--- src/github.com/microsoft/vscode-remote-try-go/hello/hello.go
+++ src/github.com/microsoft/vscode-remote-try-go/hello/hello.go
@@ -2,7 +2,22 @@ package hello
 
 import ()
 
+
+type user struct {
+	ID   int64
+	Name string
+	Addr *address
+}
+
+type address struct {
+	City   string
+	ZIP    int
+	LatLng [2]float64
+}
+
+var alex = user{}
+
 // Hello writes a welcome string
 func Hello() string {
-	return "Hello golang!"
+	return "Hello, "+alex.Name
 }

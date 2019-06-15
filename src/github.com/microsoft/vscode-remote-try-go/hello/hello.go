54d7510529219537537d5afcd964f283646ad35f
Alexander Krieg <alitari67@gmail.com>
2019-06-15 12:10:20 +0000

    Make structs public to demonstrate  add tags

--- src/github.com/microsoft/vscode-remote-try-go/hello/hello.go
+++ src/github.com/microsoft/vscode-remote-try-go/hello/hello.go
@@ -2,22 +2,23 @@ package hello
 
 import ()
 
-
-type user struct {
+// User user type
+type User struct {
 	ID   int64
 	Name string
-	Addr *address
+	Addr *Address
 }
 
-type address struct {
+// Address address type
+type Address struct {
 	City   string
 	ZIP    int
 	LatLng [2]float64
 }
 
-var alex = user{}
+var alex = User{}
 
 // Hello writes a welcome string
 func Hello() string {
-	return "Hello, "+alex.Name
+	return "Hello, " + alex.Name
 }
